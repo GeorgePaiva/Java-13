06e10c78c596ce598a61e9fba90e72b8a05237af
GeorgePaiva <george.paiva15@gmail.com>
2020-03-09 08:45:27 -0300

    Import

---
+++ src/fundamentos/Import.java
@@ -0,0 +1,24 @@
+package fundamentos;
+
+import java.sql.Date;
+
+import javax.swing.JButton;
+
+public class Import {
+
+	public static void main(String[] args) {
+
+		java.lang.String b = "Boa tarde!";
+		java.lang.System.out.println(b);
+
+		String s = "Bom dia!";
+		System.out.println(s);
+
+		Date d = new Date(0);
+		System.out.println(d);
+
+		JButton botao = new JButton();
+
+	}
+
+}
